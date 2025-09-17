2e53ab26440716d4f4607712138f34cfcdf19d40
Matías Barrios <matias.barriosn@gmail.com>
2025-09-17 00:45:51 -0300

    backup google with sdk

--- src/config.rs
+++ src/config.rs
@@ -7,5 +7,10 @@ impl AppConfig {
     pub const SPOTIFY_CLIENT_ID: &'static str = "a57ab1ceee1f4094b55924d3e228ae53";
     /// The public client ID for the Google Drive API, used for the backup feature.
     pub const GOOGLE_CLIENT_ID: &'static str =
-        "48225261965-t4hfd2n53gtl9qvj9sqh03vtobv5dboh.apps.googleusercontent.com";
+        "84904078589-ads8pq2rols27kj3c3q4bv1aac5u8i7v.apps.googleusercontent.com";
+    
+    /// Gets the Google Client Secret from environment variable
+    pub fn google_client_secret() -> Option<String> {
+        std::env::var("GOOGLE_CLIENT_SECRET").ok()
+    }
 }
